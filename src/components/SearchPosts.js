6e380ea70790d51d266de2848d52153a4cadec98
goranc <cabarkapa.goran@gmail.com>
2021-04-08 12:13:18 +0200

    Changed class to className in searchField

--- src/components/SearchPosts.js
+++ src/components/SearchPosts.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const SearchPosts = () => {
   return (
-    <div class="input-group mb-3">
-      <span class="input-group-text" id="basic-addon1">
+    <div className="input-group mb-3">
+      <span className="input-group-text" id="basic-addon1">
         Search
       </span>
       <input
         type="text"
-        class="form-control"
+        className="form-control"
         placeholder="Author"
         aria-label="Author"
         aria-describedby="basic-addon1"
