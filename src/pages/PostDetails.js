c7c346d09e921d7ef1855a7f0e725a75ada49a7c
goranc <cabarkapa.goran@gmail.com>
2021-04-20 12:41:23 +0200

    Stying the PostDetails component

--- src/pages/PostDetails.js
+++ src/pages/PostDetails.js
@@ -4,13 +4,23 @@ import { useFetchPost } from 'context/PostsContext';
 
 import Post from 'components/Post';
 import Loader from 'components/Loader';
-
+import styled from 'styled-components';
 const PostDetails = () => {
   const { id } = useParams();
   const { isLoading, selectedPost } = useFetchPost(id);
   console.log(selectedPost);
   console.log(isLoading);
-  return <div>{isLoading ? <Loader /> : <Post post={selectedPost} />}</div>;
+  return (
+    <StyledPostDetails>
+      {isLoading ? <Loader /> : <Post post={selectedPost} />}
+    </StyledPostDetails>
+  );
 };
 
+const StyledPostDetails = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
 export default PostDetails;
